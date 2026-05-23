fbc39abb7dad6a2e705b09571cef524a6ff96953
samthepessimist <231116144@mycput.ac.za>
2026-05-24 01:44:36 +0200

    registered policies in AuthServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,21 @@
 
 namespace App\Providers;
 
+use App\Models\Course;
+use App\Models\Enrolment;
+use App\Models\User;
+use App\Policies\CoursePolicy;
+use App\Policies\EnrolmentPolicy;
+use App\Policies\StudentPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Gate::policy(Course::class, CoursePolicy::class);
+        Gate::policy(Enrolment::class, EnrolmentPolicy::class);
+        Gate::policy(User::class, StudentPolicy::class);
     }
 }
